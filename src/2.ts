00238ef788e115659b6ee996d935feb7a3233180
Motkova Kateryna <sirenkoekaterina90@gmail.com>
2023-11-25 14:49:28 +0200

    1

--- src/2.ts
+++ src/2.ts
@@ -15,6 +15,31 @@
 */
 
 class Employee {
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number
+  ) {}
+
+  getEmployeeDetails() {
+    return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+  }
+}
+
+class Manager extends Employee {
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
+  }
+}
+
+export {};
+
+
+
+
+
+/*
+class Employee {
   // Заповніть модифікатори доступу
   name: string;
   department: string;
@@ -31,9 +56,13 @@ class Employee {
   }
 }
 
+
+
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
 }
 
 
 export {};
+
+*/
